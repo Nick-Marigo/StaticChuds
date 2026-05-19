6f44f17df2d366c4eace8d1c14593bf017485485
Nick Marigo <nmarigo@ucsc.edu>
2026-05-18 20:10:52 -0700

    Add functions to load classes json data and derserialize it

--- Assets/Scripts/Player/ClassLoader.cs
+++ Assets/Scripts/Player/ClassLoader.cs
@@ -1,14 +1,51 @@
 using UnityEngine;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System;
 
-public class ClassLoader
+public class ClassesLoader
 {
 
     private static Dictionary<string, List<Classes>> classes;
 
+    public static Dictionary<string, List<Classes>> GetClasses()
+    {
+        if (classes == null)
+        {
+            classes = LoadClasses();
+        }
+        return classes;
+    }
 
+    private static Dictionary<string, List<Classes>> LoadClasses()
+    {
+        TextAsset classesJson = Resources.Load<TextAsset>("classes");
+
+        if (classesJson == null)
+        {
+            Debug.Log("Failed to get classes json from Resources");
+            return null;
+        }
+
+        int status = ClassesLoader.JsonToList(classesJson.text, out classes);
+        if (status == -1)
+        {
+            Debug.Log("Failed to load levels from Json");
+            return null;
+        }
+
+        return classes;
+    }
+
+    private static int JsonToList(string json, out Dictionary<string, List<Classes>> classesData)
+    {
+        classesData = JsonConvert.DeserializeObject<Dictionary<string, List<Classes>>>(json);
+
+        if (classesData == null)
+        {
+            return -1;
+        }
+
+        return 0;
+    }
 
 }
