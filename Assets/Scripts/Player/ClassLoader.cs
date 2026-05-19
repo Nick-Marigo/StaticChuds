24125df85e9b9ce1f41d12295756ea0ef2835153
Nick Marigo <nmarigo@ucsc.edu>
2026-05-18 18:04:07 -0700

    Setup basic classes template

---
+++ Assets/Scripts/Player/ClassLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
+
+public class ClassLoader
+{
+
+    private static Dictionary<string, List<Classes>> classes;
+
+
+
+}
---
+++ Assets/Scripts/Player/Classes.cs
@@ -0,0 +1,11 @@
+using Newtonsoft.Json;
+
+[System.Serializable]
+public class Classes {
+    public string sprite;
+    public string health;
+    public string mana;
+    public string mana_regeneration;
+    public string spellPower;
+    public string speed;
+}
