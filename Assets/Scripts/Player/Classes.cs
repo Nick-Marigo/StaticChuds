ce01548dc08c983f2e639acf7581bd359e05599d
Nick Marigo <nmarigo@ucsc.edu>
2026-05-19 00:18:58 -0700

    Added RPNEvaluator calculations to classes

--- Assets/Scripts/Player/Classes.cs
+++ Assets/Scripts/Player/Classes.cs
@@ -1,4 +1,4 @@
-using Newtonsoft.Json;
+using System.Collections.Generic;
 
 [System.Serializable]
 public class Classes {
@@ -8,4 +8,29 @@ public class Classes {
     public string mana_regeneration;
     public string spellpower;
     public string speed;
+
+    public int CalculateHP(int wave)
+    {
+        return RPNEvaluator.RPNEvaluator.Evaluate(health, new Dictionary<string, int> { {"wave", wave} });
+    }
+
+    public int CalculateMana(int wave)
+    {
+        return RPNEvaluator.RPNEvaluator.Evaluate(mana, new Dictionary<string, int> { {"wave", wave} });
+    }
+
+    public int CalculateManaRegeneration(int wave)
+    {
+        return RPNEvaluator.RPNEvaluator.Evaluate(mana_regeneration, new Dictionary<string, int> { {"wave", wave} });
+    }
+
+    public int CalculateSpellPower(int wave)
+    {
+        return RPNEvaluator.RPNEvaluator.Evaluate(spellpower, new Dictionary<string, int> { {"wave", wave} });
+    }
+
+    public int CalculateSpeed(int wave)
+    {
+        return RPNEvaluator.RPNEvaluator.Evaluate(speed, new Dictionary<string, int> { {"wave", wave} });
+    }
 }
