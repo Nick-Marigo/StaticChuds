ce31e0888d6d2b9bf23cfdbd49798a22ecbc4b64
Max Kinet <max.a.kinet@gmail.com>
2026-05-21 12:54:39 -0700

    add relic fetching and equipping

--- Assets/Scripts/Relics/RelicInventory.cs
+++ Assets/Scripts/Relics/RelicInventory.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections.Generic;
 
 public class RelicInventory {
     EntityAttributePackage _attributePackage;
 
-    Dictionary<string, Relic> _potentialRelics;
+    Dictionary<string, Func<Relic>> _potentialRelics;
     Dictionary<string, Relic> _equippedRelics;
 
     public RelicInventory (EntityAttributePackage attributePackage) {
-        _potentialRelics = new();
+        _potentialRelics = RelicLoader.Relics;
         _equippedRelics = new();
+
         _attributePackage = attributePackage;
     }
 
-    
+    public Relic FetchUnusedRelic() {
+        Relic relic = _potentialRelics["green_gem"](); 
+        _potentialRelics.Remove("green_gem");
+        return relic;
+    }
 
-
+    public void EquipRelic(Relic relic) {
+       relic.attributePackageRequested += () => relic.SetAttributePackage(_attributePackage.GetAttributes()); 
+    }
 }
