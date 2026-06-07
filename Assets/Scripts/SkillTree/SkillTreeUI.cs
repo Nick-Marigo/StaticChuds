7523869b65cb041f86cc746000ed38e66f255679
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-06-07 14:50:20 -0700

    Was able to get root to spawn

--- Assets/Scripts/SkillTree/SkillTreeUI.cs
+++ Assets/Scripts/SkillTree/SkillTreeUI.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 public class SkillTreeUI : MonoBehaviour
 {
-    private SkillTreeUI skillTree;
+    private SkillTree skillTree;
+
+    [SerializeField] RectTransform skillTreeContent;
+    [SerializeField] GameObject nodePrefab;
+
+
 
     void Start()
     {
         skillTree = new SkillTree();
+        Node root = new Node("root", "root", 0, "root");
+
+        GameObject rootObject = Instantiate(nodePrefab, skillTreeContent);
+
+        NodeUI rootUI = rootObject.GetComponent<NodeUI>();
+        rootUI.SetNode(root);
     }
 
-    
 }
