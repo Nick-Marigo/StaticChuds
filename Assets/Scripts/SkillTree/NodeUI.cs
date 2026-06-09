aa913374b91ad86e8f62fd713563fab7e5a154b4
Nick Marigo <nmarigo@ucsc.edu>
2026-06-08 20:15:02 -0700

    Added description display when hovering over a skill

--- Assets/Scripts/SkillTree/NodeUI.cs
+++ Assets/Scripts/SkillTree/NodeUI.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
-public class NodeUI : MonoBehaviour
+public class NodeUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI descriptionText;
 
     private Node node;
     private SkillTreeUI skillTreeUI;
@@ -16,6 +18,7 @@ public class NodeUI : MonoBehaviour
         this.skillTreeUI = skillTreeUI;
 
         nameText.text = node.name;
+        descriptionText.text = node.description;
     }
 
     public void NodeClicked()
@@ -25,6 +28,22 @@ public class NodeUI : MonoBehaviour
             node.isPurchased = true;
             RectTransform rect = GetComponent<RectTransform>();
             skillTreeUI.NodeClicked(node, rect.anchoredPosition);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (descriptionText.gameObject != null)
+        {
+            descriptionText.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (descriptionText.gameObject != null)
+        {
+            descriptionText.gameObject.SetActive(false);
         }
     }
 
