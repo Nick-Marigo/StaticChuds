7943e8995050c0eb59750ada3dc1b43ef33f4da1
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-06-09 22:02:32 -0700

    Place new relics to left and right of first relic, instead of placing all to the right of first

--- Assets/Scripts/UI/RelicUIManager.cs
+++ Assets/Scripts/UI/RelicUIManager.cs
@@ -24,12 +24,23 @@ public class RelicUIManager : MonoBehaviour
         {
             GameObject relicUIObj = Instantiate(relicUIPrefab, transform);
 
-            relicUIObj.transform.localPosition = new Vector3(40 * i, 0, 0);
+            relicUIObj.transform.localPosition = new Vector3(GetRelicPlacementOffset(i), 0, 0);
             RelicUI relicUI = relicUIObj.GetComponent<RelicUI>();
 
             relicUI.SetRelicDisplay(relic);
             relicUIObjects.Add(relicUIObj);
             i++;
         }
+    }
+    
+    private int GetRelicPlacementOffset(int relicIndex) {
+        // Places first in middle then alternates placement on right then left
+        // Relic index == # of relics owned before obtaining
+        if (relicIndex == 0) return 0;
+			
+        int sign = (relicIndex % 2) * 2 - 1; // right is signed positive, left is signed negative
+        int offsetShift = (relicIndex + 1) / 2; // Actual offset from index=0
+			
+        return (sign * offsetShift) * 40; // 40 pixels of spacing between object origins
     }
 }
