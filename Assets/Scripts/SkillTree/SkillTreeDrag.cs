4a503dc3215b0514594ff7cb86b2006ad42b07e0
Nick Marigo <nmarigo@ucsc.edu>
2026-06-10 20:39:57 -0700

    Clamped how far the user can scroll on the skill tree.

--- Assets/Scripts/SkillTree/SkillTreeDrag.cs
+++ Assets/Scripts/SkillTree/SkillTreeDrag.cs
@@ -4,9 +4,25 @@ using UnityEngine.EventSystems;
 public class SkillTreeDrag : MonoBehaviour, IDragHandler
 {
     [SerializeField] private RectTransform skillTreeContent;
+    [SerializeField] private RectTransform viewport;
 
     public void OnDrag(PointerEventData eventData)
     {
         skillTreeContent.anchoredPosition += eventData.delta;
+        ClampPosition();
+    }
+
+    private void ClampPosition()
+    {
+        Vector2 contentSize = skillTreeContent.rect.size;
+        Vector2 viewportSize = viewport.rect.size;
+
+        float maxX = (contentSize.x - viewportSize.x) /2f;
+        float maxY = (contentSize.y - viewportSize.y) / 2f;
+
+        float clampedX = Mathf.Clamp(skillTreeContent.anchoredPosition.x, -maxX, maxX);
+        float clampedY = Mathf.Clamp(skillTreeContent.anchoredPosition.y, -maxY, maxY);
+
+        skillTreeContent.anchoredPosition = new Vector2(clampedX, clampedY);
     }
 }
